d468442cfe25f3c62668a0b63c5a91a9815dc315
noinvis <lazizbek.sn@gmail.com>
2025-07-19 04:11:22 +0500

    Finish

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,9 +3,15 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <header className="sticky top-0 z-10 bg-slate-900">
-      <nav className="flex gap-[3rem] h-[90px] items-center justify-center text-white">
-        <NavLink className="text-[28px]" to={"/"}>Home</NavLink>
-        <NavLink className="text-[28px]" to={"/usermain"}>Users</NavLink>
+      <nav className=" flex h-[90px] items-center justify-center">
+        <ul className="flex gap-[3rem] text-white">
+          <li>
+            <NavLink className="text-[28px]" to={"/"}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink className="text-[28px]" to={"/usermain"}>Users</NavLink>
+          </li>
+        </ul>
       </nav>
     </header>
   );
